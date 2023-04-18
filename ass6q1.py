2f6b2aed5a205f9f3c6071f6b03057a2a6f04933
Himanshu Keshri <114506407+Hishu455@users.noreply.github.com>
2023-04-18 21:40:10 +0530

    Add files via upload

---
+++ ass6q1.py
@@ -0,0 +1,13 @@
+# 1. Create a JSON file (employee.json) containing employee information of minimum 5 employees. 
+#    Each employee information consists of Name, DOB, Height, City, State. Write a python program that reads 
+#    this information from the JSON file and saves the information into a list of objects of Employee class. 
+#    Finally print the list of the Employee objects.
+
+import json
+
+file = open(E:"\python\ass6")
+data = json.load(file)
+for i in data["employee_details"]:
+    print(f'Name = {i["name"]} \nDOB={i["DOB"]} \nHeight={i["Height"]} \nCity={i["City"]} \nState={i["State"]} \n')
+
+
